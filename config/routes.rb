4a3d4250191289a966f11876d251c4af6c572823
Michael Guse <guse.michael@gmail.com>
2011-03-08 22:10:18 -0500

    Finished first cut of he user model

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ SampleApp::Application.routes.draw do
   # get "pages/help"
   # get "users/new"
   
+  resources :users
+
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
