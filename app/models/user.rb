c5aee0195852da0a8e7fccb9a2c30aac60a8da0a
Michael Guse <guse.michael@gmail.com>
2011-03-09 23:49:52 -0500

    Added authentication to User class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,18 @@
 # == Schema Information
-# Schema version: 20110308162535
+# Schema version: 20110310033935
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
+#  salt               :string(255)
 #
 
+require 'digest'
 class User < ActiveRecord::Base
   attr_accessor :password
   attr_accessible :name, :email, :password, :password_confirmation
@@ -27,4 +30,37 @@ class User < ActiveRecord::Base
   validates :password,	:presence     => true,
 			:length       => { :within => 6..40 },
 			:confirmation => true
+
+  before_save :encrypt_password
+
+  # Return true if the user's password matches the submitted password.
+  def has_password?(submitted_password)
+       encrypted_password == encrypt(submitted_password)
+  end
+
+  def self.authenticate(email, submitted_password)
+    user = find_by_email(email)
+    return nil  if user.nil?
+    return user if user.has_password?(submitted_password)
+  end
+
+  private
+
+    def encrypt_password
+	self.salt = make_salt if new_record?
+	self.encrypted_password = encrypt(password)
+    end
+
+    def make_salt
+	secure_hash("#{Time.now.utc}--#{password}")
+    end
+
+    def encrypt(string)
+	secure_hash("#{salt}--#{string}")
+    end
+
+    def secure_hash(string)
+	Digest::SHA2.hexdigest(string)
+    end
+
 end
